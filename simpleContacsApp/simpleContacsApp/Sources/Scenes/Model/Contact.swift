83853886eaf67a76c96a81b264c7311be959e918
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-11-24 16:58:13 +1000

    create model

---
+++ simpleContacsApp/simpleContacsApp/Sources/Scenes/Model/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  simpleContacsApp
+//
+//  Created by Georgiy on 24.11.2022.
+//
+
+import Foundation
+
+struct Contact {
+    let name:String?
+    let jobTitle:String?
+    let country:String?
+}
