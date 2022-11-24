957e03cc7ac8edfd922336ffb92a1a1365a9256e
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-11-24 17:00:16 +1000

    create VC

--- simpleContacsApp/simpleContacsApp/Sources/Scenes/Controller/ContactViewController.swift
+++ simpleContacsApp/simpleContacsApp/Sources/Scenes/Controller/ContactViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ContactViewController: UIViewController {
-
+     
+    private let contacts = ContactAPI.getContacts()
+    
+    private var settingsView: SettingsView? {
+        guard isViewLoaded else { return nil }
+        return view as? SettingsView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureView()
     }
-
-
+    
+    override func loadView() {
+        view = SettingsView()
+    }
 }
 
+extension ContactViewController {
+    func configureView() {
+        self.navigationItem.title = "Contacts"
+        
+        settingsView?.contactsTableView.dataSource = self
+        settingsView?.contactsTableView.delegate = self
+    }
+}
+
+extension ContactViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath) as! ContactTableViewCell
+        
+        cell.configure(with: contacts[indexPath.row])
+        return cell
+    }
+}
+
+extension ContactViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
